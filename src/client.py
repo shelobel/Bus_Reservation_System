d10340c21a73962eadc09d1a20357131899dd358
Shaily Shekhar <shailyshekhar123@gmail.com>
2023-12-11 21:46:42 +0530

    comments added

--- src/client.py
+++ src/client.py
@@ -1,20 +1,25 @@
 import socket
 import hashlib
 
+# Function to register a new user
 def register_user():
     username = input("Enter a username: ")
     password = input("Enter a password: ")
+    # generates hashed password for authentication
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     
+    # Append the new user information to the users.txt file
     with open("users.txt", "a") as file:
         file.write(f"{username}:{hashed_password}\n")
     print("Registration successful. You can now login.")
 
+# Function to authenticate and login a user
 def login_user():
     username = input("Enter your username: ")
     password = input("Enter your password: ")
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     
+    # Check if the provided username and hashed password match any stored credentials
     with open("users.txt", "r") as file:
         for line in file:
             stored_username, stored_password = line.strip().split(":")
@@ -23,6 +28,7 @@ def login_user():
     return False
 
 def main():
+    # Connect to the server
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("192.168.0.179", 8888))
 
@@ -35,8 +41,10 @@ def main():
         choice = input("\n\nEnter your choice: ")
 
         if choice == "1":
+            # Call the register_user function for user registration
             register_user()
         elif choice == "2":
+            # Call the login_user function for user authentication
             if login_user():
                 print("Login successful.")
                 break
@@ -49,12 +57,14 @@ def main():
             print("Invalid choice. Please try again.")
 
     while True:
+        # User interaction for booking seats, displaying matrix, or exiting
         action = input("\n                 ****** WELCOME TO BOOKBUS ****** \n 1) Type \"display\" if you would like to display the seat matrix \n\n 2) Type \"book <seat_number>\" to book your desired seat \n\n 3) Type \"exit\" to exit from terminal\n")
         client.send(action.encode())
         
+        # Receive and display the price of a seat
         price = client.recv(1024).decode()
        
-        
+        # Receive and display the server's response
         response = client.recv(1024).decode()
         print(response)
         print(f"\n\nPrice of a seat is: Rs {price}\n\n")
@@ -63,6 +73,7 @@ def main():
         elif action == "exit":
             break
 
+    # Close the client socket when done
     client.close()
 
 if __name__ == "__main__":
